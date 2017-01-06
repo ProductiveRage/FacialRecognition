9a48a35a7f638e4cd68696da17246b28e89f0efa
ProductiveRage <dangger36@gmail.com>
2017-01-06 13:26:47 +0000

    Scale larger images down to make the face searching faster in testing

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -13,18 +13,58 @@ namespace Tester
 			var filename = "TigerWoods.gif";
 			var outputFilename = "Output.png";
 
+			// This strikes a reasonable balance between successfully matching faces in my current (small) sample data while performing the work quickly
+			const int maxAllowedSize = 480;
+
 			var config = DefaultConfiguration.Instance;
 			var timer = new IntervalTimer(Console.WriteLine);
 			var faceDetector = new FaceDetector(config, timer.Log);
 			using (var source = new Bitmap(filename))
 			{
-				var faceRegions = faceDetector.GetPossibleFaceRegions(source.GetRGB());
+				var largestDimension = Math.Max(source.Width, source.Height);
+				var scaleDown = (largestDimension > maxAllowedSize) ? ((double)largestDimension / maxAllowedSize) : 1;
+				var colourData = (scaleDown > 1) ? GetResizedBitmapData(source, scaleDown) : source.GetRGB();
+
+				var faceRegions = faceDetector.GetPossibleFaceRegions(colourData);
+				if (scaleDown > 1)
+					faceRegions = faceRegions.Select(region => Scale(region, scaleDown));
 				WriteOutputFile(outputFilename, source, faceRegions, Color.GreenYellow);
 				timer.Log($"Complete (written to {outputFilename}), {faceRegions.Count()} region(s) identified");
 			}
 			Console.WriteLine();
 			Console.WriteLine("Press [Enter] to terminate..");
 			Console.ReadLine();
+		}
+
+		private static DataRectangle<RGB> GetResizedBitmapData(Bitmap source, double scale)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (scale <= 0)
+				throw new ArgumentOutOfRangeException(nameof(scale));
+
+			using (var resizedSource = GetResizedBitmap(source, scale))
+			{
+				return resizedSource.GetRGB();
+			}
+		}
+
+		private static Bitmap GetResizedBitmap(Bitmap source, double scale)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (scale <= 0)
+				throw new ArgumentOutOfRangeException(nameof(scale));
+
+			return new Bitmap(source, new Size((int)Math.Round(source.Width / scale), (int)Math.Round(source.Height / scale)));
+		}
+
+		private static Rectangle Scale(Rectangle region, double scale)
+		{
+			if (scale <= 0)
+				throw new ArgumentOutOfRangeException(nameof(scale));
+
+			return new Rectangle((int)Math.Round(region.X * scale), (int)Math.Round(region.Y * scale), (int)Math.Round(region.Width * scale), (int)Math.Round(region.Height * scale));
 		}
 
 		private static void WriteOutputFile(string outputFilename, Bitmap source, IEnumerable<Rectangle> faceRegions, Color outline)
