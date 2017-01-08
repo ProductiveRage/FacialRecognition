f4ec7d25a65fb66fe3e9987c235278adf5b58f8e
ProductiveRage <dangger36@gmail.com>
2017-01-08 14:58:36 +0000

    Need to read "P5" as well as "P2" (aka "plain") PGM files (also added way to read only dimensions, for examining the training data)

--- Tester/PGMReader.cs
+++ Tester/PGMReader.cs
@@ -9,7 +9,7 @@ namespace Tester
 {
 	public static class PGMReader
 	{
-		public static DataRectangle<byte> ReadPlainPGM(string path)
+		public static DataRectangle<byte> ReadPGM(string path)
 		{
 			if (string.IsNullOrWhiteSpace(path))
 				throw new ArgumentException($"Null/blank {nameof(path)} specified");
@@ -18,19 +18,96 @@ namespace Tester
 			{
 				using (var reader = new BinaryReader(stream))
 				{
-					return ReadPlainPGM(reader);
+					return ReadPGM(reader);
 				}
 			}
 		}
 
-		public static DataRectangle<byte> ReadPlainPGM(BinaryReader reader)
+		public static Size ReadPGMDimensions(string path)
+		{
+			if (string.IsNullOrWhiteSpace(path))
+				throw new ArgumentException($"Null/blank {nameof(path)} specified");
+
+			using (var stream = File.OpenRead(path))
+			{
+				using (var reader = new BinaryReader(stream))
+				{
+					return ReadPGMDimensions(reader);
+				}
+			}
+		}
+
+		public static Size ReadPGMDimensions(BinaryReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			var versionAndDimensionsAndRange = ReadVersionAndDimensionsAndDynamicRage(reader);
+			return new Size(versionAndDimensionsAndRange.Item2.Width, versionAndDimensionsAndRange.Item2.Height);
+		}
+
+		public static DataRectangle<byte> ReadPGM(BinaryReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			var versionAndDimensionsAndRange = ReadVersionAndDimensionsAndDynamicRage(reader);
+			var width = versionAndDimensionsAndRange.Item2.Width;
+			var height = versionAndDimensionsAndRange.Item2.Height;
+
+			var pixelArray = new byte[width, height];
+			if (versionAndDimensionsAndRange.Item1 == "P2")
+			{
+				var pixels = new List<byte>();
+				while (true)
+				{
+					var line = TryToReadToEndOfLine(reader);
+					if (line == null)
+						break;
+					foreach (var segment in line.Split())
+					{
+						byte value;
+						if (!byte.TryParse(segment, out value))
+							throw new ArgumentException($"Invalid image content, encountered non-byte segment ({segment})");
+						pixels.Add(value);
+					}
+				}
+				if (pixels.Count != (width * height))
+					throw new ArgumentException($"Invalid content, read {pixels.Count} pixels instead of the expected {width * height}");
+				for (var y = 0; y < height; y++)
+				{
+					for (var x = 0; x < width; x++)
+						pixelArray[x, y] = pixels[(y * width) + x];
+				}
+			}
+			else
+			{
+				for (var y = 0; y < height; y++)
+				{
+					for (var x = 0; x < width; x++)
+					{
+						try
+						{
+							pixelArray[x, y] = reader.ReadByte();
+						}
+						catch
+						{
+							throw new ArgumentException($"Invalid image content, expected {width * height} pixels but ran out of content at {(y * width) + x}");
+						}
+					}
+				}
+			}
+			return DataRectangle.For(pixelArray);
+		}
+
+		private static Tuple<string, Size, byte> ReadVersionAndDimensionsAndDynamicRage(BinaryReader reader)
 		{
 			if (reader == null)
 				throw new ArgumentNullException(nameof(reader));
 
 			var header = TryToReadToEndOfLine(reader);
-			if (header != "P2")
-				throw new ArgumentException("Magic number missing (only support 'P2' plain PGM files) - invalid content");
+			if ((header != "P2") && (header != "P5"))
+				throw new ArgumentException("Magic number missing - invalid content");
 
 			Size? dimensions = null;
 			int? range = null;
@@ -63,33 +140,11 @@ namespace Tester
 					throw new ArgumentException("Invalid dynamic range value, only support values 1-255 (inclusive)");
 				break;
 			}
-
-			var pixels = new List<byte>();
-			while (true)
-			{
-				var line = TryToReadToEndOfLine(reader);
-				if (line == null)
-					break;
-				foreach (var segment in line.Split())
-				{
-					byte value;
-					if (!byte.TryParse(segment, out value))
-						throw new ArgumentException($"Invalid image content, encountered non-byte segment ({segment})");
-					pixels.Add(value);
-				}
-			}
-			var width = dimensions.Value.Width;
-			var height = dimensions.Value.Height;
-			if (pixels.Count != (width * height))
-				throw new ArgumentException($"Invalid content, read {pixels.Count} pixels instead of the expected {width * height}");
-
-			var pixelArray = new byte[width, height];
-			for (var y = 0; y < height; y++)
-			{
-				for (var x = 0; x < width; x++)
-					pixelArray[x, y] = pixels[(y * width) + x];
-			}
-			return DataRectangle.For(pixelArray);
+			if (dimensions == null)
+				throw new ArgumentException("Invalid file content, could not retrieve dimensions");
+			if (range == null)
+				throw new ArgumentException("Invalid file content, could not retrieve dynamic range");
+			return Tuple.Create(header, new Size(dimensions.Value.Width, dimensions.Value.Height), (byte)range.Value);
 		}
 
 		private static Size ParseDimensions(string line)
