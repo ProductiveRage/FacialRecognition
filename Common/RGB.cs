cbb81384c080ba269707f473c21529cc9ef049c2
ProductiveRage <dangger36@gmail.com>
2017-01-11 21:56:11 +0000

    It didn't make sense having ToGreyScale be an extension method for RGB, it should have been an instance method

--- Common/RGB.cs
+++ Common/RGB.cs
@@ -14,9 +14,15 @@
 		public byte R { get; }
 		public byte G { get; }
 		public byte B { get; }
+
 		public override string ToString()
 		{
 			return $"RGB:{R}:{G}:{B}";
+		}
+
+		public double ToGreyScale()
+		{
+			return (0.2989 * colour.R) + (0.5870 * colour.G) + (0.1140 * colour.B); // Greyscale formula
 		}
 	}
 }
