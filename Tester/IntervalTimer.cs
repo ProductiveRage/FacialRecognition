91d1302cc4a8217736f7617e4bec7a106b5587e2
ProductiveRage <dangger36@gmail.com>
2017-01-28 14:42:36 +0000

    Tweak to formatting of IntervalTimer (include thousand separators)

--- Tester/IntervalTimer.cs
+++ Tester/IntervalTimer.cs
@@ -6,8 +6,7 @@ namespace Tester
 	public sealed class IntervalTimer
 	{
 		private readonly Action<string> _writeTo;
-		private readonly Stopwatch _timerSinceLastEvent;
-		private readonly Stopwatch _timer;
+		private readonly Stopwatch _timerSinceLastEvent, _timer;
 		public IntervalTimer(Action<string> writeTo)
 		{
 			if (writeTo == null)
@@ -17,6 +16,7 @@ namespace Tester
 			_timerSinceLastEvent = Stopwatch.StartNew();
 			_timer = Stopwatch.StartNew();
 		}
+
 		public void Log(string message)
 		{
 			if (string.IsNullOrWhiteSpace(message))
@@ -25,7 +25,7 @@ namespace Tester
 			var timeSinceLastEvent = _timerSinceLastEvent.Elapsed;
 			var totalTimeSoFar = _timer.Elapsed;
 			_timerSinceLastEvent.Restart();
-			_writeTo($"[{Math.Round(timeSinceLastEvent.TotalMilliseconds)}ms / {Math.Round(totalTimeSoFar.TotalMilliseconds)}ms] {message}");
+			_writeTo($"[{(timeSinceLastEvent.TotalMilliseconds).ToString("N0")}ms / {(totalTimeSoFar.TotalMilliseconds).ToString("N0")}ms] {message}");
 		}
 	}
 }
